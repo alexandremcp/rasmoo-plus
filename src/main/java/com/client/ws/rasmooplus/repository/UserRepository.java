3a719097839edbe7eb684c11901991d55fcd0df2
alexandremcp <alexandremcp@gmail.com>
2023-02-17 19:33:21 -0300

    Criando as classes Repository

---
+++ src/main/java/com/client/ws/rasmooplus/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.client.ws.rasmooplus.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.client.ws.rasmooplus.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {  
+}
---
+++ src/main/java/com/client/ws/rasmooplus/repository/UserTypeRepository.java
@@ -0,0 +1,10 @@
+package com.client.ws.rasmooplus.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.client.ws.rasmooplus.model.UserType;
+
+@Repository
+public interface UserTypeRepository extends JpaRepository<UserType, Long> {  
+}
